08447facc9502680c6b1d68e937e85f8f70f63d6
BlazeIsClone <sandevabeykoon123@gmail.com>
2023-10-21 21:54:26 +0530

    update: add input selection for ssh key file

--- src/db_backup.rs
+++ src/db_backup.rs
@@ -7,6 +7,7 @@ pub fn init(
     remote_hostname: &str,
     remote_username: &str,
     local_dir: &str,
+    private_key_path: &str,
 ) -> Result<(), Box<dyn std::error::Error>> {
     let mysql_db: String = Input::new()
         .with_prompt("MySQL database")
@@ -37,17 +38,23 @@ pub fn init(
         &mysql_host, &mysql_port, &mysql_user, &mysql_password, &mysql_databases, &mysql_output
     );
 
-    let session = tcp_session(remote_hostname, remote_username)?;
+    match tcp_session(remote_hostname, remote_username, private_key_path) {
+        Ok(session) => {
+            println!("SSH session established successfully.");
 
-    session.channel_session()?.exec(&mysqldump_cmd)?;
+            session.channel_session()?.exec(&mysqldump_cmd)?;
 
-    let _ = sftp_write_local(&session, &mysql_output, &local_dir, "database.sql");
+            let _ = sftp_write_local(&session, &mysql_output, &local_dir, "database.sql");
 
-    // Remove temp file
-    println!("Removing temp file: {}", &mysql_output);
-    let rm_cmd = format!("rm -f /var/tmp/backup-cli-{}-database.sql", &remote_file_id);
+            // Remove temp file
+            println!("Removing temp file: {}", &mysql_output);
+            let rm_cmd = format!("rm -f /var/tmp/backup-cli-{}-database.sql", &remote_file_id);
 
-    session.channel_session()?.exec(&rm_cmd)?;
+            session.channel_session()?.exec(&rm_cmd)?;
+        }
+
+        Err(err) => eprintln!("SSH session error: {}", err),
+    }
 
     Ok(())
 }
--- src/tcp_session.rs
+++ src/tcp_session.rs
@@ -6,12 +6,11 @@ use std::path::Path;
 pub fn tcp_session(
     remote_hostname: &str,
     remote_username: &str,
+    private_key_path: &str,
 ) -> Result<Session, Box<dyn Error>> {
     // SSH connection information
     let port = 22;
     let current_username = whoami::username();
-    let private_key_path = format!("/home/{current_username}/.ssh/id_rsa");
-
     // Connect to the SSH server
     let tcp = TcpStream::connect((remote_hostname, port))?;
     let mut session = Session::new()?;
@@ -19,8 +18,15 @@ pub fn tcp_session(
     session.handshake()?;
 
     // Authenticate with the SSH server using the private key
-    session.userauth_pubkey_file(&remote_username, None, Path::new(&private_key_path), None)?;
+    session.userauth_pubkey_file(
+        &remote_username,
+        None,
+        Path::new(&format!(
+            "/home/{}/.ssh/{}",
+            &current_username, &private_key_path
+        )),
+        None,
+    )?;
 
-    // Execute a command on the remote server
     Ok(session)
 }
